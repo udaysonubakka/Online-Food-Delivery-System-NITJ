7713f1e997e48af0de384abf7cfb7b46781e6ed0
udaysonubakka <udaysonubakka143@gmail.com>
2020-07-02 15:56:02 +0530

    add basic MVC structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const app=express()
+const PORT=8000
+app.get("/",function(req,res){
+    res.send("<h1>Server started succesfully </h1>")
+})
+
+app.listen(PORT,function(err){
+    if(err)
+    {console.log("Error in starting port")}
+    else{console.log("Server started successfully");}
+    return
+})
